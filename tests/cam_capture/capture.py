9e5a0c1fdfad7ec8b2c21166a306b1653dbb928f
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-05-21 18:36:16 +0200

    Commenting capture.py

--- tests/cam_capture/capture.py
+++ tests/cam_capture/capture.py
@@ -1,30 +1,70 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from PIL import Image
 from io import BytesIO
 
 class CameraClient:
     SNAP_ENDPOINT = "/web/tmpfs/snap.jpg"
     WEB_PORT = 80
 
-    def __init__(self, host, login="admin", password="admin"):
+    def __init__(self, host, username="admin", password="admin"):
+        """
+        Creates a CameraClient which could be used to capture frame from the camera
+        
+        Arguments:
+            host {str} -- The ip or hostname of the camera  
+        
+        Keyword Arguments:
+            username {str} -- The username to use to connect (default: {"admin"})
+            password {str} -- The password to use to connect (default: {"admin"})
+        """
         self.host = host
-        self.login = login
+        self.username = username
+
         self.password = password
 
-        self.auth = HTTPBasicAuth(self.login, self.password)
+        # Creating a basic authentification from the arguments
+        self.auth = HTTPBasicAuth(self.username, self.password)
 
+        # Defining the http request url which can be used to request an image to the camera
         self.url = "http://{}:{}{}".format(host, self.WEB_PORT, self.SNAP_ENDPOINT)
-        print(self.url)
 
-    def capture_raw(self):
+    def capture_raw(self): 
+        """
+        Could be used to get a single image from the camera. It is described by bytes.
+        A skimage/opencv image (a simple [x, y, 3] numpy array) can be easily created with io.imread(camera.capture_raw()).
+        
+        Returns:
+            BytesIO -- the byte stream describing the image
+
+        Raises:
+            RequestException -- if the connection was not succesfully completed, for ex. if the host can not be reached.
+            
+        """
         response = requests.get(self.url, auth=self.auth)
+        if response.status_code == 401: # bad credentials
+            raise self.BadCredentialsError()
+        elif not str(response.status_code).startswith("2"): # bad response code
+            raise self.BadResponseFormat()
+
         content = response.content
         response.close()
         return BytesIO(content)
 
+    # ------- ERRORS ------- #
+    class Error(Exception):
+        pass
+
+    class BadCredentialsError(Error):
+        def __init__(self):
+            super().__init__("The credentials cannot be used to connect to the camera")
+
+    class BadResponseFormat(Error):
+        def __init__(self):
+            super().__init__("Something was wrong with the camera response")
+
+
 if __name__ == "__main__":
-    # if not used as a module: testing
+    # here are some tests when not used as a module
     from skimage import io
     from skimage.viewer import ImageViewer
 
