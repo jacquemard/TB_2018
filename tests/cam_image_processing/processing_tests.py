95edcb115308a1c10838e799ee4d3f00633c08b5
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-05-22 17:26:06 +0200

    exploring edge_detection and downsampling from an open image done

---
+++ tests/cam_image_processing/processing_tests.py
@@ -0,0 +1,91 @@
+from skimage import io, transform, filters, exposure, util, color
+from skimage.viewer.viewers import ImageViewer, CollectionViewer
+from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
+
+# Loading the image. This is a numpy array with a [x, y, 3] shape
+img = io.imread("park.jpg")
+
+# Downsampling tests
+# The image is 1280x720px, which is a 16/9 ratio
+# Note: img is an 3D array where each line represents an image line. Therefore, accessing
+# the first dimension design a line, and accessing the second dimension design a pixel
+# in this line. So, for an x*y pixels image, its shape is (y, x, 3).
+min_size = (90, 160)
+
+def get_downsampled_images(image):
+    current_size = min_size
+
+    downsampled_images = []
+
+    while current_size[0] <= image.shape[0]:
+        downsampled_image = transform.resize(image, current_size, mode='reflect')
+
+        downsampled_images.append(downsampled_image)
+        
+        current_size = (current_size[0] + min_size[0], current_size[1] + min_size[1])
+
+    return downsampled_images
+
+# done for each chanel (adapted from http://scikit-image.org/docs/0.12.x/auto_examples/color_exposure/plot_adapt_rgb.html)    
+@adapt_rgb(each_channel)
+def fil_each(i, fil):    
+    # We use 1 - filters(image)
+    # but this will not work if image is not normalized
+    return fil(i)
+
+@adapt_rgb(hsv_value)
+def fil_hsv(i, fil):
+    return fil(i)
+
+def fil_gray(i, fil):
+    return fil(color.rgb2gray(i))
+
+def get_edges_images(image):    
+    fils = [filters.sobel, filters.scharr]
+
+    funcs = [fil_each, fil_hsv, fil_gray]
+
+    edge_images = []
+    for fil in fils:        
+        for func in funcs:
+            edge_images.append(exposure.rescale_intensity(1 - func(image, fil)))
+
+    return edge_images
+
+#imgs = get_edges_images(img)
+#CollectionViewer(imgs).show()
+#imgs = get_downsampled_images(img)
+#CollectionViewer(imgs).show()
+
+"""
+# ---------- 1) edge, 2) downsample
+# The get_edges_images(img)[4] seems better (scharr filter, hsv values)
+edge_image = exposure.rescale_intensity(1 - fil_hsv(img, filters.scharr))
+# Exploring downsampling
+downsampled_images = get_downsampled_images(edge_image)
+CollectionViewer(downsampled_images).show()
+# The downsampled_images[2] seems ok (270x480)
+"""
+
+# ---------- 1) downsample, 2) edge
+"""
+# The get_downsampled_images(img)[1] seems ok (180x320)
+#downsampled_image = transform.resize(img, (180, 320), mode='reflect')
+# Exploring edge detections
+#edge_images = get_edges_images(downsampled_image)
+#CollectionViewer(edge_images).show()
+# Scharr hsv filter seems better
+# Exploring differents downsampling with scharr hsv filter
+images = list(map(lambda i: exposure.rescale_intensity(1 - fil_hsv(i, filters.scharr)), get_downsampled_images(img)))
+CollectionViewer(images).show()
+# images[2] seems better (270x480), with scharr filter
+"""
+
+# ---------- downsample or edge detection first ?
+edge_first = exposure.rescale_intensity(1 - fil_hsv(img, filters.scharr))
+edge_first = transform.resize(edge_first, (270, 480), mode='reflect')
+
+downsample_first = transform.resize(img, (270, 480), mode='reflect')
+downsample_first = exposure.rescale_intensity(1 - fil_hsv(downsample_first, filters.scharr))
+
+CollectionViewer([edge_first, downsample_first]).show()
