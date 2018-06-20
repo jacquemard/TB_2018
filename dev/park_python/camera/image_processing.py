be61ee674ec24d7dfc609c6f67f6b42a040917a5
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-06-20 16:41:31 +0200

    TODO: use image_processing file within the camera/recorder.py to improove modularity

---
+++ dev/park_python/camera/image_processing.py
@@ -0,0 +1,31 @@
+import skimage
+from skimage import io, transform, exposure, filters
+from skimage.color.adapt_rgb import adapt_rgb, hsv_value
+
+IMAGE_OUTPUT_SIZE = (270, 480)
+
+'''
+We are using a scharr filter to detect edges. 
+The @adapt_rgb is a skimage annotation which adapts any grayscale filters to a per 
+channel basis
+The 'image' will be separated by channel (using the hsv color space here)
+and the filter will be applied to each of them. The final image results of a concatenation
+of these channels.
+'''
+@adapt_rgb(hsv_value)
+def _scharr_hsv(image):
+    return filters.scharr(image)
+
+# Defining what to do when an image is received
+def process_image(image_stream):
+    # Converting to a skimage/opencv image (simply a [x, y, 3] numpy array)
+    image = io.imread(image_stream)
+
+    # Firstly, downsampling the image
+    image = transform.resize(image, IMAGE_OUTPUT_SIZE, mode='reflect')
+
+    # Secondly, detecting the edges
+    image = _scharr_hsv(image)
+
+    # return the processed image
+    return image
