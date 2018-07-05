951f2ef1a2343f7178eabb53fe4bac23b3a90fea
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-07-05 15:15:35 +0200

    cars_anotation minor modif

--- dev/park_python/drafts/object_detection/tensorflow_api/cars_annotation.py
+++ dev/park_python/drafts/object_detection/tensorflow_api/cars_annotation.py
@@ -43,13 +43,13 @@ for annotation in content['annotations'][0]:
     size_elem = ElementTree.Element("size")
     root.append(size_elem)
     width_elem = ElementTree.Element("width")
-    width_elem.text = image.shape[0]
+    width_elem.text = str(image.shape[0])
     size_elem.append(width_elem)
     height_elem = ElementTree.Element("height")
-    height_elem.text = image.shape[1]
+    height_elem.text = str(image.shape[1])
     size_elem.append(height_elem)
     depth_elem = ElementTree.Element("depth")
-    depth_elem.text = image.shape[2]
+    depth_elem.text = str(image.shape[2])
     size_elem.append(depth_elem)
     # -- segmented
     segmented_elem = ElementTree.Element("segmented")
