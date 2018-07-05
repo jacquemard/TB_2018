e2e3e5360b43224698826b1c33059b5a0a555311
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-07-05 14:49:41 +0200

    added mat file to voc xml file converter for the car dataset

---
+++ dev/park_python/drafts/object_detection/tensorflow_api/cars_annotation.py
@@ -0,0 +1,81 @@
+import scipy.io as sio
+from pathlib import Path
+from skimage import io
+from xml.etree import ElementTree 
+import sys, os
+
+HOME_PATH = str(Path.home())
+
+MAT_FILE = HOME_PATH + "/DS/cars/cars_annos.mat"
+CARS_PATH = HOME_PATH + "/DS/cars/imgs"
+XML_PATH = HOME_PATH + "/DS/cars/annotations"
+os.makedirs(XML_PATH, exist_ok=True)
+
+# loading the mat (MATLAB) file
+content = sio.loadmat(MAT_FILE)
+
+for annotation in content['annotations'][0]:
+    # getting info
+    filename = annotation[0][0].split('/')[-1]
+
+    xmin = str(annotation[1][0][0])
+    ymin = str(annotation[2][0][0])
+    xmax = str(annotation[3][0][0])
+    ymax = str(annotation[4][0][0])
+
+    image = io.imread(CARS_PATH + "/" + filename)
+    
+    print("{}:{},{},{},{}".format(filename, xmin, ymin, xmax, ymax))
+
+    # creating xml VOC file
+    root = ElementTree.Element("annotation")
+    # -- filename
+    filename_elem = ElementTree.Element("filename")
+    filename_elem.text = filename
+    root.append(filename_elem)
+    # -- size
+    
+    size_elem = ElementTree.Element("size")
+    root.append(size_elem)
+    width_elem = ElementTree.Element("width")
+    width_elem.text = image.shape[0]
+    size_elem.append(width_elem)
+    height_elem = ElementTree.Element("height")
+    height_elem.text = image.shape[1]
+    size_elem.append(height_elem)
+    depth_elem = ElementTree.Element("depth")
+    depth_elem.text = image.shape[2]
+    size_elem.append(depth_elem)
+    
+    # -- segmented
+    segmented_elem = ElementTree.Element("segmented")
+    segmented_elem.text = str(0)
+    root.append(segmented_elem)
+    # -- car object
+    object_element = ElementTree.Element("object")
+    root.append(object_element)
+
+    name_elem = ElementTree.Element("name")
+    name_elem.text = "car"
+    object_element.append(name_elem)
+
+    bounds_elem = ElementTree.Element("bndbox")
+    object_element.append(bounds_elem)
+
+    xmin_elem = ElementTree.Element("xmin")
+    xmin_elem.text = xmin
+    bounds_elem.append(xmin_elem)
+    ymin_elem = ElementTree.Element("ymin")
+    ymin_elem.text = ymin
+    bounds_elem.append(ymin_elem)
+    xmax_elem = ElementTree.Element("xmax")
+    xmax_elem.text = xmax
+    bounds_elem.append(xmax_elem)
+    ymax_elem = ElementTree.Element("ymax")
+    ymax_elem.text = ymax
+    bounds_elem.append(ymax_elem)
+
+    #ElementTree.dump(root)
+    tree = ElementTree.ElementTree(root)
+    tree.write(XML_PATH + "/" + filename.split('.')[0] + ".xml")
+    break
