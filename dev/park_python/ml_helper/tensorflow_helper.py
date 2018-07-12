8a6891c36bb79cafefdd3d81be8551449ed3f8eb
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-07-12 21:38:00 +0200

    updating tensorflow_helper

--- dev/park_python/ml_helper/tensorflow_helper.py
+++ dev/park_python/ml_helper/tensorflow_helper.py
@@ -1,13 +1,12 @@
 import os
 
-
-def create_trainval_file(path, ext, output_file):
+def create_trainval_file(input_path, ext, output_file):
      # creating the trainval text file
     trainval_file = open(output_file, "w")
 
-    for root, _, files in os.walk(dataset_path):
+    for _, _, files in os.walk(input_path):
         for f in files:
             if f.endswith(ext):
                 # Adding the image to trainvals
-                trainval_file.write(file.split('.')[-2] + "\n")
+                trainval_file.write(f.split('.')[-2] + "\n")
     trainval_file.close()
