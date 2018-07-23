54ab64ba5a9faf5fd95234b38cf2a86f0ae570ed
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-07-23 21:18:44 +0000

    yolo_predictor added

--- dev/park_python/ml_helper/yolo_predictor.py
+++ dev/park_python/ml_helper/yolo_predictor.py
@@ -1,13 +1,19 @@
 import darknet
 
 class YoloPredictor:
-    CONFIG_PATH = "yolov3.cfg"
-    META_PATH = "yolov3.data"
+    CONFIG_PATH = b"yolov3.orig.cfg"
+    META_PATH = b"coco.data"
 
     def __init__(self, weights):
-        self.net = darknet.load_net(self.CONFIG_PATH, weights, 0)
+        self.net = darknet.load_net(self.CONFIG_PATH, bytes(weights, encoding='utf-8'), 0)
         self.meta = darknet.load_meta(self.META_PATH)
 
-    def predict_image_file(self, image_file):
-        detection = darknet.detect(self.net, self.meta, image_file)
-        print(detection)
+    def predict_num_cars(self, image_file):
+        detection = darknet.detect(self.net, self.meta, bytes(image_file, encoding='utf-8'))
+        n_cars = 0
+
+        for object in detection:
+            if object[0] == b'car':
+                 n_cars += 1
+
+        return n_cars
