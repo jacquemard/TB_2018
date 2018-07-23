1b0f6c61a72ad0621d6bf3938da186961a876559
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-07-23 11:52:43 +0000

    tests dev: minor modif

---
+++ dev/park_python/drafts/object_detection/tensorflow_api_pklot/eval.sh
@@ -0,0 +1,5 @@
+python /home/ubuntu/tensorflow_models/research/object_detection/eval.py \
+--logtostderr \
+--checkpoint_dir=/home/ubuntu/DS/PKLot/tensorflow_ds/training/  \
+--pipeline_config_path=/home/ubuntu/TB/GIT/dev/park_python/drafts/object_detection/tensorflow_api_pklot/training.config \
+--eval_dir=/home/ubuntu/DS/PKLot/tensorflow_ds/eval_pklotfull_16000/
--- dev/park_python/drafts/object_detection/tensorflow_api_pklot/inference_graph.sh
+++ dev/park_python/drafts/object_detection/tensorflow_api_pklot/inference_graph.sh
@@ -1,6 +1,6 @@
 python /home/ubuntu/tensorflow_models/research/object_detection/export_inference_graph.py \
 --input_type=image_tensor \
 --pipeline_config_path=training.config \
---trained_checkpoint_prefix=/home/ubuntu/DS/PKLot/tensorflow_ds/training/model.ckpt-16279 \
---output_directory=/home/ubuntu/DS/PKLot/tensorflow_ds/inference_pklot_full
+--trained_checkpoint_prefix=/home/ubuntu/DS/PKLot/tensorflow_ds/training_pklotfull_4000/model.ckpt-4000 \
+--output_directory=/home/ubuntu/DS/PKLot/tensorflow_ds/inference_pklot_full_4000
 
